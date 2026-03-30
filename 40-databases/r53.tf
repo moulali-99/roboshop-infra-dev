816f3c63cd7be197efa5458fb15c10e41e2ffa96
moulali pinjari <moulali22p@gmail.com>
2026-03-30 14:02:19 +0530

    update

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,11 +1,12 @@
 resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
-  name    = "mongodb-${var.environment}-${var.domain_name}"
+  name    = "mongodb-${var.environment}.${var.domain_name}"
   type    = "A"
-  ttl     = 1
+  ttl     = "1"
   records = [aws_instance.mongodb.private_ip]
   allow_overwrite = true
 }
+
 
 resource "aws_route53_record" "redis" {
   zone_id = var.zone_id
