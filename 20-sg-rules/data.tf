1de5c454e4d3f7fc2a1abed096068ded27fe3ea1
moulali pinjari <moulali22p@gmail.com>
2026-03-30 14:27:23 +0530

    actions

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -14,9 +14,15 @@ data "aws_ssm_parameter" "bastion_sg_id" {
 data "aws_ssm_parameter" "mongodb_sg_id" {
     name="/${var.project}/${var.environment}/mongodb_sg_id"
 }
+
+data "aws_ssm_parameter" "redis_sg_id" {
+    name="/${var.project}/${var.environment}/redis_sg_id"
+}
+
 data "aws_ssm_parameter" "catalouge_sg_id" {
     name="/${var.project}/${var.environment}/catalouge_sg_id"
 }
+
 data "aws_ssm_parameter" "user_sg_id" {
     name="/${var.project}/${var.environment}/user_sg_id"
 }
