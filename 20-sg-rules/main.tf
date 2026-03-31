ebf35db43c9c0ba46c0debc2dc72b8743a761b43
moulali pinjari <moulali22p@gmail.com>
2026-03-31 12:02:41 +0530

    update

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group_rule" "mongodb_catalouge" {
   to_port           = 27017
   protocol          = "tcp"
   # Where traffic is coming from
-  source_security_group_id = local.catalogue_sg_id
+  source_security_group_id = local.catalouge_sg_id
   security_group_id = local.mongodb_sg_id
 }
 
